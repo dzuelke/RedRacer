9029cd54b710637fc7c8c03d3419482e2d2a64fd
Eric Brisco <eric.brisco@gmail.com>
2009-06-07 20:11:19 +0000

    CHG: RedracerUserUniqueValidator has been adapted to replace RedracerEmailValidator. See documentation.
    DEL: RedracerEmailValidator
    
    closes #33

--- app/modules/User/lib/validator/RedracerUserUniqueValidator.class.php
+++ app/modules/User/lib/validator/RedracerUserUniqueValidator.class.php
@@ -8,10 +8,17 @@
 // +---------------------------------------------------------------------------+
 
 /**
- * Validates 
+ * Validates that a given user attribute is unique in the database
+ *
+ * Parameters:
+ *  'attribute'     The user attribute to compare the data against. If not
+ *                  provided, it defaults to the argument name.
+ *  'can_match_current_user'    Allows the data to match the attribute of the
+ *                              current user.
  *
  * @copyright  (c) the authors
  * @author     Benjamin Boerngen-Schmidt <benjamin@boerngen-schmidt.de>
+ * @author     Eric Brisco <erisco@abstractflow.com>
  * @copyright  Authors
  * @license    GPLv3
  * @package    User
@@ -22,20 +29,37 @@
 class RedracerUserUniqueValidator extends AgaviValidator {
 	public function validate()
 	{
-		$argument = $this->getArgument();
-		$data = $this->getData($argument);
-		
-		/**
-		 * @var UserManagerModel
-		 */
-		$um = $this->getContext()->getModel('UserManager');
-		
-		if($um->isUnique($argument, $data)) {
-			return true;
-		}
-		
-		$this->throwError();
-		return false;
+        if ($this->hasParameter('attribute')) {
+            $attribute = $this->getParameter('attribute');
+        } else {
+            $attribute = $this->getArgument();
+        }
+
+		$data = $this->getData($this->getArgument());
+        $context = $this->getContext();
+
+        $okay = false;
+
+        if ($this->hasParameter('can_match_current_user')
+            && $this->getParameter('can_match_current_user')) {
+            $userinfo = $context->getUser()->getAttribute('userinfo');
+            if (isset($userinfo[$attribute]) && $userinfo[$attribute] == $data) {
+                $okay = true;
+            }
+        }
+
+        if (!$okay) {
+            /**
+             * @var UserManagerModel
+             */
+            $um = $context->getModel('UserManager');
+            if(!$um->isUnique($attribute, $data)) {
+                $this->throwError();
+                return false;
+            }
+        }
+
+        return true;
 	}
 }
 ?>
