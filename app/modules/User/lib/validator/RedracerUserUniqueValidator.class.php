a4ef4e376e742892d8aea50189c2ed9a6a3113da
Eric Brisco <eric.brisco@gmail.com>
2009-06-08 21:50:11 +0000

    CHG: Reverting changes in revision 39 to RedracerUserUniqueValidator

--- app/modules/User/lib/validator/RedracerUserUniqueValidator.class.php
+++ app/modules/User/lib/validator/RedracerUserUniqueValidator.class.php
@@ -8,17 +8,10 @@
 // +---------------------------------------------------------------------------+
 
 /**
- * Validates that a given user attribute is unique in the database
- *
- * Parameters:
- *  'attribute'     The user attribute to compare the data against. If not
- *                  provided, it defaults to the argument name.
- *  'can_match_current_user'    Allows the data to match the attribute of the
- *                              current user.
+ * Validates 
  *
  * @copyright  (c) the authors
  * @author     Benjamin Boerngen-Schmidt <benjamin@boerngen-schmidt.de>
- * @author     Eric Brisco <erisco@abstractflow.com>
  * @copyright  Authors
  * @license    GPLv3
  * @package    User
@@ -29,37 +22,20 @@
 class RedracerUserUniqueValidator extends AgaviValidator {
 	public function validate()
 	{
-        if ($this->hasParameter('attribute')) {
-            $attribute = $this->getParameter('attribute');
-        } else {
-            $attribute = $this->getArgument();
-        }
-
-		$data = $this->getData($this->getArgument());
-        $context = $this->getContext();
-
-        $okay = false;
-
-        if ($this->hasParameter('can_match_current_user')
-            && $this->getParameter('can_match_current_user')) {
-            $userinfo = $context->getUser()->getAttribute('userinfo');
-            if (isset($userinfo[$attribute]) && $userinfo[$attribute] == $data) {
-                $okay = true;
-            }
-        }
-
-        if (!$okay) {
-            /**
-             * @var UserManagerModel
-             */
-            $um = $context->getModel('UserManager');
-            if(!$um->isUnique($attribute, $data)) {
-                $this->throwError();
-                return false;
-            }
-        }
-
-        return true;
+		$argument = $this->getArgument();
+		$data = $this->getData($argument);
+		
+		/**
+		 * @var UserManagerModel
+		 */
+		$um = $this->getContext()->getModel('UserManager');
+		
+		if($um->isUnique($argument, $data)) {
+			return true;
+		}
+		
+		$this->throwError();
+		return false;
 	}
 }
 ?>
